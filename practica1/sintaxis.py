65ca2912edbb37a1d035c0a09e6c29b96cd1905f
Spica56 <andresmartinezbadillo55@gmail.com>
2025-05-16 18:19:32 -0600

    Se agregaron variables
    Se agregaron algunas cadenas y nuevas variables

--- practica1/sintaxis.py
+++ practica1/sintaxis.py
@@ -13,3 +13,24 @@ buajajaja
 
 print("hola soy una cadena :D")
 print('yo soy otra cadena igualita')
+
+variable1 = "hola, soy una cadena"
+print(len(variable1))
+print(variable1[2:5])
+print(variable1[2:])
+print(variable1[:5])
+
+#3 Variables
+
+x= "ivan issay"
+x= 4
+x= 5.76
+
+print(x)
+
+x= int(3)
+y= float(3)
+z= str(3)
+
+print(x, y, z)
+print(type(x))
