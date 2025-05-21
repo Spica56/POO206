4c046fc596ed693793504bd77dd17ec1284b09c9
Spica56 <andresmartinezbadillo55@gmail.com>
2025-05-21 17:43:11 -0600

    Uso de raise

--- Practica2/Excepciones.py
+++ Practica2/Excepciones.py
@@ -18,7 +18,7 @@ except (ValueError, ZeroDivisionError) as e:
     print("Error: Se ingreso algo que no es un número entero." if isinstance(e, ValueError) else "Error: Estas intentando dividir entre 0.")
 """
 #Uso de finally
-
+"""
 try:
     numero = int(input("introduce un número: "))
     resultado = 10 /numero
@@ -26,3 +26,16 @@ except (ValueError, ZeroDivisionError) as e:
     print("Error: Se ingreso algo que no es un número entero." if isinstance(e, ValueError) else "Error: Estas intentando dividir entre 0.")
 finally:
     print("Gracias por ejecutar el programa.")
+"""
+#Uso de raise
+
+def verificar_edad(edad):
+    if edad < 18:
+        raise ValueError("Debes ser mayor de edad.")
+    print("Acceso permitido.")
+
+try:
+    verificar_edad(16)
+except ValueError as e:
+    print(f"Error: {e}")
+
