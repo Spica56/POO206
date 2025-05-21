ad9b50076171d5f0a80c5a599ca4391e3384ef15
Spica56 <andresmartinezbadillo55@gmail.com>
2025-05-21 17:38:42 -0600

    Uso de finally

--- Practica2/Excepciones.py
+++ Practica2/Excepciones.py
@@ -8,7 +8,7 @@ except ValueError:
         print("Error: Se ingreso algo que no es un numero entero,")
 except ZeroDivisionError:
       print("Error: Estas intentando dividir entre 0")
-
+"""
 # Multiples excepciones en una linea
 """
 try:
@@ -16,4 +16,13 @@ try:
     resultado = 10 / numero
 except (ValueError, ZeroDivisionError) as e:
     print("Error: Se ingreso algo que no es un número entero." if isinstance(e, ValueError) else "Error: Estas intentando dividir entre 0.")
+"""
+#Uso de finally
 
+try:
+    numero = int(input("introduce un número: "))
+    resultado = 10 /numero
+except (ValueError, ZeroDivisionError) as e:
+    print("Error: Se ingreso algo que no es un número entero." if isinstance(e, ValueError) else "Error: Estas intentando dividir entre 0.")
+finally:
+    print("Gracias por ejecutar el programa.")
