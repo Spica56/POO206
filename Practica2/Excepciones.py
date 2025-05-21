59972769d6dee36b543c6d9ddde18b724afe130b
Spica56 <andresmartinezbadillo55@gmail.com>
2025-05-21 15:28:31 -0600

    Multiples excepciones
    Se agrego un ejemplo de múltiples excepciones en una línea.

--- Practica2/Excepciones.py
+++ Practica2/Excepciones.py
@@ -1,4 +1,4 @@
-try:
+"""try:
     numero = int(input("Introduce un número: "))
     resultado = 10/numero
     
@@ -8,3 +8,12 @@ except ValueError:
         print("Error: Se ingreso algo que no es un numero entero,")
 except ZeroDivisionError:
       print("Error: Estas intentando dividir entre 0")
+
+# Multiples excepciones en una linea
+"""
+try:
+    numero = int(input("Introduce un número: "))
+    resultado = 10 / numero
+except (ValueError, ZeroDivisionError) as e:
+    print("Error: Se ingreso algo que no es un número entero." if isinstance(e, ValueError) else "Error: Estas intentando dividir entre 0.")
+
