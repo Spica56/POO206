1d09976c8ce4ba7efdf4c79a4203e1819f2d17fd
Spica56 <andresmartinezbadillo55@gmail.com>
2025-05-22 20:11:19 -0600

    Año Bisiesto
    Se añadió un programa que pide un año e indica si el año es bisiesto o no.

---
+++ practica3/bisiesto.py
@@ -0,0 +1,17 @@
+#Año bisiesto
+
+x= 5
+while x<=5:
+    try:
+        a = int(input("Introduce un año: "))
+        if(a%4==0):
+            if(a%100==0 and a%400 !=0):
+                print("No es un año bisiesto")
+            else:
+                print("Es un año bisiesto.")
+        else:
+            print("No es un año bisiesto.")
+    except (ValueError):
+        print("Error: No se ingreso un numero entero.")
+    finally:
+        print("Los años bisiestos tienen 366 días y se añaden en febrero.")
