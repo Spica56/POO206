0f55cd5b4afbd3ec404a03a0d19dfcbe3ec312db
Spica56 <andresmartinezbadillo55@gmail.com>
2025-06-20 18:45:54 -0600

    Manejo de excepciones
    Se añadieron mensajes de error por cada excepción y se evito que se borraran todos los campos cuando ocurre un error.

--- flaskProyect/app.py
+++ flaskProyect/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template, request, url_for, flash, redirect
 from flask_mysqldb import MySQL
 import MySQLdb
 
@@ -9,6 +9,7 @@ app.config['MYSQL_USER']= "root"
 app.config['MYSQL_PASSWORD']= "Badillo83"
 app.config['MYSQL_DB']= "dbflask"
 #app.config['MYSQL_PORT']= 3306 //solo si se cambia el puerto
+app.secret_key='mysecretkey'
 
 mysql= MySQL(app)
 
@@ -24,7 +25,48 @@ def DB_check():
 
 @app.route('/')
 def home():
-    return 'Hola mundo Flask'
+    return render_template('formulario.html')
+
+@app.route('/consulta')
+def consulta():
+    return render_template('consulta.html')
+
+#Ruta para la Insert
+@app.route('/guardarAlbum', methods = ['POST'])
+def guardar():
+
+    #lista de errores
+    errores={}
+
+    #obtener los datos a insertar
+    Vtitulo= request.form.get('txtTitulo', '').strip()
+    Vartista= request.form.get('txtArtista', '').strip()
+    Vanio= request.form.get('txtAnio', '').strip()
+
+    if not Vtitulo:
+        errores['txtTitulo']= 'Nombre del album obligatorio'
+    if not Vartista:
+        errores['txtArtista']= 'Artista es obligatorio'
+    if not Vanio:
+        errores['txtAnio']= 'Año es obligatorio'
+    elif not Vanio.isdigit() or int(Vanio)< 1800 or int(Vanio)> 2030:
+        errores['txtAnio']= 'Ingresa un año valido'
+
+    if not errores:
+        #Intentamos ejecutar el INSERT
+        try:
+            cursor= mysql.connection.cursor()
+            cursor.execute('insert into tb_album(album,artista,anio) values(%s,%s,%s)', (Vtitulo, Vartista, Vanio))
+            mysql.connection.commit()
+            flash('Album se guardo en BD')
+            return redirect(url_for('home'))
+        except Exception as e:
+            mysql.connection.rollback()
+            flash('Algo fallo: '+ str(e))
+            return redirect(url_for('formulario'))
+        finally:
+            cursor.close()
+    return render_template('formulario.html', errores= errores)
 
 if __name__ == '__main__':
     app.run(port= 3000, debug= True)
